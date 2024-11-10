33987ff9b6534d0414bb881bca964ada342532bd
Zeugi22 <pentzelp@yahoo.com>
2024-11-10 22:07:15 +0100

    reverted setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup
 setup(
     name='math_quiz',
     version='0.1',
-    packages=['math_quiz'],
-    py_modules=['math_quiz.py', 'tests_math_quiz.py']
+    packages=['math_quiz']
 )
