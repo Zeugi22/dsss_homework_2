462847c8d0a3228feca52a46a0b3cab042d883e9
Zeugi22 <pentzelp@yahoo.com>
2024-11-10 21:46:53 +0100

    added py-modules= to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='math_quiz',
     version='0.1',
-    packages=['math_quiz']
+    packages=['math_quiz'],
+    py_modules=['math_quiz.py', 'tests_math_quiz.py']
 )
