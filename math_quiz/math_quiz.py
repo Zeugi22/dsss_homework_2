218b0a5a81e9a145588e1294834939ad8f3f4efd
Zeugi22 <pentzelp@yahoo.com>
2024-11-10 15:06:39 +0100

    committed modified math_quiz

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,78 @@
 import random
 
+def create_random_number(min, max):
+    """Returns a random integer in a range from min to max including both."""
 
-def function_A(min, max):
-    """
-    Random integer.
-    """
     return random.randint(min, max)
 
+def choose_operation():
+    """Randomly returns one of the given operations '+', '-' and '*' as a string."""
 
-def function_B():
     return random.choice(['+', '-', '*'])
 
+def generate_problem(nr1, nr2, op):
+    """
+    Creates a mathematical problem and its answer from two numbers and an operator.
+    
+    Args:
+        nr1 (int): first number
+        nr2 (int): second number
+        op (string): operator
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+    Returns:
+        (problem, answer):
+        problem (string): composed of all three args, answer (int): correct answer to the problem.
+    """
+    #create problem as f-string
+    problem = f"{nr1} {op} {nr2}"
+
+    #calculate answer to problem by differentiating the operator
+    if op == '+': answer = nr1 + nr2
+    elif op == '-': answer = nr1 - nr2
+    else: answer = nr1 * nr2
+
+    return problem, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Starts a quiz containing four simple mathematical problems.
+
+    -four problems
+    -userinput requested for each problem
+    -one chance to answer for each problem
+    -score is being tracked and printed at the end
+    """
+    score = 0
+    number_of_problems = 4
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    #loop for the 4 problems
+    for _ in range(number_of_problems):
+        #create the 3 parts of a problem
+        nr1 = create_random_number(1, 10); nr2 = create_random_number(1, 6); op = choose_operation()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = generate_problem(nr1, nr2, op)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
+        #loop to ensure correct input format
+        while(1):
+            useranswer = input("Your answer: ")
+            try:
+                useranswer = int(useranswer)
+            except:
+                print("invalid numberformat: please enter an integer!")
+            else:
+                break
+        
+        #checking, if useranswer is correct, and edit score, if needed
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{number_of_problems}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,35 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import create_random_number, choose_operation, generate_problem
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_create_random_number(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = create_random_number(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_choose_operation(self):
+        for _ in range(1000):
+            op = choose_operation()
+            self.assertIn(op, ('+', '-', '*'))
+    
+    def test_generate_problem(self):
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (10, 6, '*', '10 * 6', 60),
+            (1, 1, '-', '1 - 1', 0),
+            (5, 2, '-', '5 - 2', 3),
+            (2, 5, '-', '2 - 5', -3),
+            (100, 100, '+', '100 + 100', 200),
+        ]
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
-
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            problem, answer = generate_problem(num1, num2, operator)
+            self.assertEqual((expected_problem, expected_answer), (problem, answer))
 
 if __name__ == "__main__":
     unittest.main()
